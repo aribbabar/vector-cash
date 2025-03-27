691916744b3aca3372dda8d26387826fdd1eb321
Arib Farooqui <aribbabar100@gmail.com>
2025-03-27 01:44:54 -0400

    improved the styling of the hero page

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [MatGridListModule, MatCardModule, MatButtonModule, RouterModule],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.scss'
 })
 export class HeroComponent {}
